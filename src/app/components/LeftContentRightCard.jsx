fff217c626e5da3cd2d0d555b7d5245bf1ee1251
babargujjar <babargujjaronline@gmail.com>
2025-06-21 17:43:14 +0500

    remove unused code

--- src/app/components/LeftContentRightCard.jsx
+++ src/app/components/LeftContentRightCard.jsx
@@ -1,77 +1,7 @@
-// import React from 'react'
-// import Button from './Button';
-// import Heading from './Heading';
-// import Image from 'next/image';
-
-// const LeftContentRightCard = () => {
-//   const card = [
-//     {
-//       img: "/assets/cardimg.svg",
-//       title: "Free consulting with every service",
-//       desc: "Get expert advice before you spend a single rupee or dollar.",
-//     },
-//     {
-//       img: "/assets/cardimg.svg",
-//       title: "Platform-Specific Growth Plans",
-//       desc: "Custom strategies tailored for Shopify, eBay, TikTok Shop, and more.",
-//     },
-//     {
-//       img: "/assets/cardimg.svg",
-//       title: "Real-Time Analytics Dashboards",
-//       desc: "Track your performance live—see what’s working, fix what’s not.",
-//     },
-//     {
-//       img: "/assets/cardimg.svg",
-//       title: "Ongoing Optimization & Support",
-//       desc: "We don’t stop after launch—our team ensures your business keeps improving.",
-//     },
-//   ];
-//   return (
-//     <div className="flex w-full py-20 gap-5">
-//       <div className="w-[45%]">
-//         <Button text="Core Features" />
-//         <Heading
-//           title="Expert Digital Commerce"
-//           highlight="Solutions"
-//           className="text-[48px] my-3"
-//         />
-//         <p className="text-[18px] text-[#71717A]">
-//           We design systems that sell while you sleep — through platforms,
-//           performance, and proven processes.
-//         </p>
-//       </div>
-//       <div className="w-[55%]">
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//           {card.map((v, i) => (
-//             <div key={i} className="border rounded-2xl p-10 border-[#CDCDCD]">
-//               <Image
-//                 className="mb-10"
-//                 src={v.img}
-//                 alt=""
-//                 width={90}
-//                 height={90}
-//               />
-//               <h2 className="font-semibold mb-3 text-black text-[24px]">
-//                 {v.title}
-//               </h2>
-//               <p className="text-[18px] text-[#71717A] ">
-//                 {v.desc}
-//               </p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default LeftContentRightCard
-
 import React from "react";
 import Button from "./Button";
 import Heading from "./Heading";
 import Image from "next/image";
-import { HugeiconsIcon, MentoringIcon } from "@hugeicons/react";
 
 const LeftContentRightCard = () => {
   const card = [
